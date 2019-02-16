58e773feea0d89066ca37ee14a35ea1657af6016
Nobu <calro1123@yahoo.co.jp>
2019-02-16 12:44:11 +0900

    na

--- tes.py
+++ tes.py
@@ -10,30 +10,33 @@ def rank(word):
 		html = urllib.request.urlopen(url)
 		soup = BeautifulSoup(html, "html.parser")
 
-				# gigazineサイトの常に最新記事のurlを取り出す
+		# gigazineサイトの常に最新記事のurlを取り出す
 		geturl = soup.find('div', attrs={'id': 'section'}).h2.a.get("href")
-				# gettitle = soup.find('div', attrs={'id': 'section'}).h2.a.get("title")
-
-				# 最新記事を開き、人気記事一覧を取得
+				
+		# 最新記事を開き、人気記事一覧を取得
 		url2= geturl
 		html2 = urllib.request.urlopen(url2)
 		soup2 = BeautifulSoup(html2, "lxml")
 
 		list=[]
+		#記事ページ内にある人気記事を表示している部分を探す
 		rank = soup2.find('div',id='ranking-entry')
+		#人気記事一覧の中のa要素を取得
 		rankli=rank.find_all('a')
 
 		for ui in rankli:
+			#人気記事のリンクを取得getu　タイトルを取得gett
 			getu=ui.get("href")
 			gett = ui.text
-			
-			# print(getu)
+		
+			#人気記事のリンク先でトップ画像を取得getimg 
 			url3= url[:-1]+getu
 			html3 = urllib.request.urlopen(url3)
 			soup3 = BeautifulSoup(html3, "lxml")
 			getcnt = soup3.find('div',{'class':'cntimage'})
 			getimg = getcnt.find('img').get('src')
-			# print(getimg)
+			
+			#リストに記事タイトル、url、画像urlを格納
 			list.append([gett,url3,getimg])
 
 	# for i in range(ranklist):
@@ -46,4 +49,4 @@ def rank(word):
 # result=rank("人気記事")
 # for arr in result:
 # 	print(arr[1])
-	# print(result[i][2],"\n")
+# 	print(result[arr][2],"\n")
